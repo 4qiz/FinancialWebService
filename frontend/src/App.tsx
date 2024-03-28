44f10e4354119bd7ae0223ef037e1be75bb23d3d
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-03-28 11:28:24 +0300

    frontend: add errorHandler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
-import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { ToastContainer } from "react-toastify/dist/components";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Outlet></Outlet>
+      <Outlet />
+      <ToastContainer />
     </>
   );
 }
---
+++ frontend/src/Services/AuthService.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { handleError } from "./ErrorHandler";
+import { UserProfileToken } from "../Models/User";
+
+const api = "http://localhost:5062/api";
+
+export const loginAPI = async (username: string, password: string) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/login", {
+      username,
+      password,
+    });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const registerAPI = async (
+  email: string,
+  username: string,
+  password: string
+) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/register", {
+      email,
+      username,
+      password,
+    });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
